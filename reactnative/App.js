c40740f42a87bb2f733b5e3bcc3154d7a48aed3a
Alexander Bay <70754958+Phatdrop@users.noreply.github.com>
2022-04-19 12:10:28 +0200

    Updated app.js
    react native, pixel 5 bug fixed

--- reactnative/App.js
+++ reactnative/App.js
@@ -1,17 +1,25 @@
 import React, {useState} from 'react';
-import { View, StyleSheet, Text, Button, Linking, DrawerLayoutAndroid } from 'react-native';
+import { View, StyleSheet, Text, Button, Linking, DrawerLayoutAndroid, Alert, Modal } from 'react-native';
 
 const App = () => {
-  const [name, setName] = useState('Style Test')
-  
-  const onClickHandler = () => {
-  setName('Opdateret style test')
+  const [name, setName] = useState('')
+  const [showWarning, SetShowWarning] = useState(false);
+  const [submitted, SetSubmitted] = useState(false);
+  const onClickHandler = () => { 
+    if (name.length > 3) {
+      SetSubmitted(!submitted);
+    } else {
+      SetShowWarning(true);
+    }
   }
 
   return ( 
     <View style={styles.body}>
       <Text style={styles.text}> {name} </Text>
       <Button title="Update state" onPress={onClickHandler} style={styles.Button}> </Button>
+      <Modal visible={showWarning}>
+        <Text> The name must be longer than 3 characters </Text>
+      </Modal>
     </View>
   );
 };
